f3b38335486ee8de916193a6471736c0e84dca4c
gurelbs <guri240@email.com>
2023-09-01 14:02:33 +0300

    feat(validate.ts): import remark

--- validators/md-files-validator/validate.ts
+++ validators/md-files-validator/validate.ts
@@ -1,18 +1,17 @@
 
 import fs from 'fs';
-// import remark
 import demands from './demands';
 import { ValidateMarkdownResponse } from './interface';
+import { remark } from 'remark';
 
-
-function meetsDemand(mdFile: string, demand: string){
-  // TODO
-}
-
-export function validateMarkdown(filePath: string): ValidateMarkdownResponse {
-  const mdFileContent = fs.readFileSync(filePath, 'utf8');
-
-
+export function validateMarkdown(mdFilePath?: string): ValidateMarkdownResponse {
+  if (!mdFilePath) {
+    console.error('[validateMarkdown] - Error: md file path cannot be empty');
+    process.exit(0);
+  }
+  const mdFile = remark().processSync(mdFilePath);
+  console.log({mdFile});
+  
   for (let demand of demands) {
     // TODO: create test...
 
