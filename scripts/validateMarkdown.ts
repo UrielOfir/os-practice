8a314b56aaaf3fcd98524a75665525c071419b61
gurelbs <guri240@email.com>
2023-09-07 08:38:49 +0300

    add env file

--- scripts/validateMarkdown.ts
+++ scripts/validateMarkdown.ts
@@ -1,9 +1,7 @@
 // import * as fs from 'fs';
 // import {default as markdownlint} from 'markdownlint';
 import { config } from 'dotenv';
-
-
-import { Octokit } from "octokit";
+import { Octokit  } from "@octokit/rest";
 
 export default async function mdFilesValidator(){
   config();
@@ -11,27 +9,24 @@ export default async function mdFilesValidator(){
     console.log('Validating docs...');
     const fetch = await import('node-fetch').then(mod => mod.default)
     const { GITHUB_TOKEN, PR_NUMBER, OWNER, REPO } = process.env;
-
-
-    console.log(process.env.GITHUB_TOKEN, PR_NUMBER, OWNER, REPO );
     
     const octokit = new Octokit({ 
       auth: GITHUB_TOKEN, 
       request: { fetch: fetch },
       userAgent: 'maakag v1.0.0',
     });
+    
+    const { data: listFiles } = await octokit.rest.pulls.listFiles({
+      owner: OWNER,
+      repo: OWNER,
+      pull_number: Number(PR_NUMBER),
+    });
 
-    const res = await octokit.request('GET /repos/{owner}/{repo}/pulls/{pull_number}/files')
-    console.log(res.data);
-    
-    const prFiles = await octokit.rest.pulls.listFiles()
-    
-    console.log('All Markdown files look valid!');
-    
-    return prFiles
+    console.log(listFiles);
+    console.log('All Markdown files look valid!');    
+    // return prFiles
   } catch (error) {
-    console.error('there was an error: ', error.message, error.stack, 'Please check your environment variables and try again');
-
+    console.error(error.message);
     process.exit(1);
   }
 }
